1f24cfe57fe2f3d88b447f8ae7eb37efba87fdab
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-01-01 12:55:56 +0100

    Year changed

--- parse.rb
+++ parse.rb
@@ -2,7 +2,7 @@ APP_PATH = File.expand_path(File.dirname(__FILE__))
 
 require "#{APP_PATH}/gtfs.rb"
 
-timetables_start_date = '2022-12-11'
-timetables_end_date = '2023-12-09'
+timetables_start_date = '2023-12-10'
+timetables_end_date = '2024-12-14'
 
 GovRoGTFSConverter::parse_and_save_for_dates(timetables_start_date, timetables_end_date)
